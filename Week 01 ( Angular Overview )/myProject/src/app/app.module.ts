6c06c2fd903d231f4abedb33fc4d661fbd4f9594
Nazhim <nazhimkalam@gmail.com>
2020-09-13 16:37:24 +0530

    created the menu page

--- Week 01 ( Angular Overview )/myProject/src/app/app.module.ts
+++ Week 01 ( Angular Overview )/myProject/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
 
@@ -19,6 +20,7 @@ import { MenuComponent } from './menu/menu.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatListModule,
     FlexLayoutModule
   ],
   providers: [],

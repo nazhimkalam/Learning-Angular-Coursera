7d5f89c0601e79ff41e33e85c281c0c7808c9c52
Nazhim <nazhimkalam@gmail.com>
2020-09-13 17:42:51 +0530

    added card components

--- Week 01 ( Angular Overview )/myProject/src/app/menu/menu.component.ts
+++ Week 01 ( Angular Overview )/myProject/src/app/menu/menu.component.ts
@@ -1,58 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
+const DISHES: Dish[] = [
+  {
+    id: '0',
+    name: 'Pizza',
+    image: '/assets/images/pizza.jpg',
+    category: 'mains',
+    featured: true,
+    label: 'Hot',
+    price: '4.99',
+    description:
+      'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+  },
+  {
+    id: '1',
+    name: 'cake',
+    image: '/assets/images/cake.jpg',
+    category: 'appetizer',
+    featured: false,
+    label: 'Sweet',
+    price: '1.99',
+    description:
+      'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+  },
+  {
+    id: '2',
+    name: 'chocolate',
+    image: '/assets/images/chocolate.jpg',
+    category: 'appetizer',
+    featured: false,
+    label: 'New',
+    price: '1.99',
+    description:
+      'A quintessential ConFusion experience, is it a vada or is it a donut?',
+  },
+  {
+    id: '3',
+    name: 'cheesecake',
+    image: '/assets/images/cheesecake.jpeg',
+    category: 'dessert',
+    featured: false,
+    label: 'Smooth',
+    price: '2.99',
+    description:
+      'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+  },
+];
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
+
+
 export class MenuComponent implements OnInit {
-  dishes: Dish[] = [
-    {
-      id: '0',
-      name: 'Pizza',
-      image: '/assets/images/pizza.jpg',
-      category: 'mains',
-      featured: true,
-      label: 'Hot',
-      price: '4.99',
-      description:
-        'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-    },
-    {
-      id: '1',
-      name: 'cake',
-      image: '/assets/images/cake.jpg',
-      category: 'appetizer',
-      featured: false,
-      label: 'Sweet',
-      price: '1.99',
-      description:
-        'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-    },
-    {
-      id: '2',
-      name: 'chocolate',
-      image: '/assets/images/chocolate.jpg',
-      category: 'appetizer',
-      featured: false,
-      label: 'New',
-      price: '1.99',
-      description:
-        'A quintessential ConFusion experience, is it a vada or is it a donut?',
-    },
-    {
-      id: '3',
-      name: 'cheesecake',
-      image: '/assets/images/cheesecake.jpeg',
-      category: 'dessert',
-      featured: false,
-      label: 'Smooth',
-      price: '2.99',
-      description:
-        'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-    },
-  ];
+  
+  dishes:Dish[] = DISHES;
+  selectedDish = DISHES[0];
+
 
   constructor() {}
 

fe3f210be07b826e1e70734b088e58f97c021cfd
Nazhim <nazhimkalam@gmail.com>
2020-09-14 12:32:57 +0530

    added service using dependency injection

--- myProject/src/app/menu/menu.component.ts
+++ myProject/src/app/menu/menu.component.ts
@@ -1,26 +1,24 @@
-import { DISHES } from './../shared/dishes';
+import { DishService } from './../services/dish.service';
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-
-
 export class MenuComponent implements OnInit {
-  
-  dishes:Dish[] = DISHES;
+  dishes: Dish[];
   selectedDish: Dish;
 
-  onSelect(dish: Dish){
+  onSelect(dish: Dish) {
     this.selectedDish = dish;
   }
 
-  constructor() {}
+  constructor(private dishService: DishService) {}
 
- 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // This is similar to a useEffect hook in react, the ngOnInit is a type of life cycle method in Angular
+    this.dishes = this.dishService.getDishes();
+  }
 }

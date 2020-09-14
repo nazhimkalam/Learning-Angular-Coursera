84b8ad10909e3f8d191ff5b7188ae5aa84251a66
Nazhim <nazhimkalam@gmail.com>
2020-09-14 16:09:25 +0530

    single page application

--- myProject/src/app/app.module.ts
+++ myProject/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -18,12 +18,12 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component'
+import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
-
 
 @NgModule({
   declarations: [
@@ -47,7 +47,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     AppRoutingModule,
     MatButtonModule,
   ],
-  providers: [DishService],
-  bootstrap: [AppComponent]
+  providers: [DishService, PromotionService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

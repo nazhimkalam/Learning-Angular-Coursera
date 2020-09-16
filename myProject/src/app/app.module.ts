a4ba190cec8b044258a5d371bd23e99de7d6a86b
Nazhim <nazhimkalam@gmail.com>
2020-09-16 13:59:01 +0530

    assignmennt task 1 & 2 completed

--- myProject/src/app/app.module.ts
+++ myProject/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatSliderModule} from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 
@@ -61,6 +62,7 @@ import { LoginComponent } from './login/login.component';
     MatProgressSpinnerModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatSliderModule,
     MatInputModule,
     MatCheckboxModule,
     FormsModule,
--- myProject/src/app/dishdetail/dishdetail.component.ts
+++ myProject/src/app/dishdetail/dishdetail.component.ts
@@ -1,10 +1,11 @@
+import { Comment } from './../shared/comment';
 import { DishService } from './../services/dish.service';
 import { Dish } from './../shared/dish';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Location } from '@angular/common';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -19,17 +20,98 @@ export class DishdetailComponent implements OnInit {
 
   // An @Output() property should normally be initialized to an Angular EventEmitter with values
   // flowing out of the component as events.
+  @ViewChild('fform') feedbackFormDirective;
+  commentForm: FormGroup;
+  comment: Comment;
 
   dish: Dish;
   dishIds: string[];
   prev: string;
   next: string;
 
+  formErrors = {
+    author: '',
+    comment: '',
+  };
+
+  validationMessages = {
+    author: {
+      required: 'Name is required.',
+      minlength: 'Name must be at least 2 characters long.',
+      maxlength: 'Name cannot be more than 25 characters long.',
+    },
+    comment: {
+      required: 'Comment is required.',
+    },
+  };
+
   constructor(
     private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location
-  ) {}
+    private location: Location,
+    private fb: FormBuilder
+  ) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.commentForm = this.fb.group({
+      author: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(25),
+        ],
+      ],
+      comment: ['', [Validators.required]],
+      date: new Date(), // we will use the pipe when displaying the content
+      rating: 5,
+    });
+
+    this.commentForm.valueChanges.subscribe((data) =>
+      this.onValueChanged(data)
+    );
+
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) {
+      return;
+    }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          console.log(messages);   // error messages displayed
+
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+
+            }
+          }
+        }
+      }
+    }
+  }
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    console.log(this.comment);
+    this.commentForm.reset({
+      author: '',
+      comment: '',
+      date: '',
+      rating: 5,
+    });
+    this.feedbackFormDirective.resetForm();
+  }
 
   ngOnInit(): void {
     this.dishservice
@@ -46,7 +128,13 @@ export class DishdetailComponent implements OnInit {
         console.log(this.dish); // gives the data if the updated dish selected when ever the route changes
         this.setPrevNext(dish.id);
       });
-    
+  }
+
+  formatLabel(value: number) {
+    // this is for the rating bar
+    console.log(value);
+    this.commentForm;
+    return value;
   }
 
   setPrevNext(dishId: string) {

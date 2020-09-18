3afc0fc6e760e3c556135d4c1c79dc45e1721aeb
Nazhim <nazhimkalam@gmail.com>
2020-09-18 22:30:21 +0530

    assignment task 1 and 2 completed

--- myProject/src/app/about/about.component.ts
+++ myProject/src/app/about/about.component.ts
@@ -1,6 +1,6 @@
 import { Leader } from './../shared/leader';
 import { LeaderService } from './../services/leader.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
@@ -15,12 +15,17 @@ import { flyInOut, expand } from '../animations/app.animation';
 })
 export class AboutComponent implements OnInit {
   leaders: Leader[];
+  leaderErrMsg: string;
 
-  constructor(private leaderService: LeaderService) {}
+  constructor(
+    private leaderService: LeaderService,
+    @Inject('BaseURL') private baseURL
+  ) {}
 
   ngOnInit(): void {
-    this.leaderService
-      .getLeaders()
-      .subscribe((result) => (this.leaders = result));
+    this.leaderService.getLeaders().subscribe(
+      (result) => (this.leaders = result),
+      (errMsg) => (this.leaderErrMsg = <any>errMsg)
+    );
   }
 }
